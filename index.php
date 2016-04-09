4b971f14a569590ae7e41035fc6cd7908b0d8dca
Tom Kawchak <Tom Kawchak>
2016-04-09 18:45:48 -0400

    update photos

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
+<?php session_start(); ?>
+
 <?php
+
+
 
 ?>
 
@@ -57,6 +61,9 @@
 <main>
 <div class="container">
 	<!-- profile image -->
+	<img src="" id="photo" class="img-responsive img-thumbnail img-square center-block" alt="food" style="max-height: 300px;">
+	
+	
 	<div class="row">
 		<div class="col-sm-3"></div>
 		<div class="col-sm-6 text-justify">
@@ -64,6 +71,27 @@
 		<div class="col-sm-3"></div>
 	</div>
 </div>
+
+
+<script>
+
+var xmlhttp;
+
+$( document ).ready(function() {
+    updatePhoto();
+});
+
+function updatePhoto() {
+	var photo;
+	$.get( "getPhoto.php", function( data ) {
+		photo = data.image;
+		$("#photo").attr("src", photo);
+	}, "json" );
+	
+}
+</script>
+
+
 </main>
 
 </body>

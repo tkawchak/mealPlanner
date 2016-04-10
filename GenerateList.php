08a65d64653a4e6ddb221acd95df07223ed4d570
adamyosua <ayosua@verizon.net>
2016-04-10 00:55:03 -0400

    fixed bug on generate list

--- GenerateList.php
+++ GenerateList.php
@@ -116,9 +116,9 @@
             if ($result->num_rows > 0) {
             // output data of each row
               while($row = $result->fetch_assoc()) {
-              echo "id: " . $row[1]. "<br>";
+                echo "id: " . $row["name"]. "<br>";
               }
-              } else {
+            } else {
                 echo "0 results";
               }
               $conn->close();

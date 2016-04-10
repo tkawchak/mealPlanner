50712f9b26b2ed41c4f7d7892de3d3ed8d7c9b1f
adamyosua <ayosua@verizon.net>
2016-04-10 01:26:55 -0400

    quered meal db for makelist.php

--- makeList.php
+++ makeList.php
@@ -1,4 +1,28 @@
 <?php
 	$num = $_POST["numberOfMeals"];
-	echo $num;
+	//echo $num;
+	
+	$servername = "localhost";
+    $username = "mealuser";
+    $password = "JyCCCFxBr3YQFyuW";
+    $dbname = "mealplanner";
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error){
+                die("Connection failed: " . $conn->connect_error);
+            } 
+            else{
+              //echo "connection worked";
+            }
+            
+            $sql = "SELECT name FROM meal";
+            $result = $conn->query($sql);
+
+            for($i = 0; $i < $num; $i++)
+			{
+				$row = $result->fetch_assoc();
+				echo "meal" . ($i + 1) . ": ". $row["name"]. "<br>";
+			}
+    $conn->close();
 ?>

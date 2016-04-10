a28dc41ae0effb736448b28985a2759671ca9d94
benkenawell <benkenawell@gmail.com>
2016-04-09 20:42:12 -0400

    finished getPhoto and added sql dump

--- getPhoto.php
+++ getPhoto.php
@@ -8,15 +8,15 @@ $files = scandir(getcwd());
 $images = array();
 
 for ($i = 0; $i < sizeof($files); $i = $i + 1) {
-	echo $files[i];
+	echo $files[$i] . "<br />";
 	if (strpos($files[$i], ".jpg") != false) {
 		array_push($images, ($files[$i]));
-		echo $files[i];
+		echo $files[$i] . "<br />";
 	}
 }
 
 // return one of the images
-$image = $images[rand(0, sizeof($images)) - 1];
+$image = $images[rand(0, sizeof($images) - 1)];
 
 //echo $image;
 

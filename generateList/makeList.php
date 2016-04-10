b53327897e52d2c52ab94e4cd5477afb75b83493
benkenawell <benkenawell@gmail.com>
2016-04-10 08:30:37 -0400

    to pull for tom

--- generateList/makeList.php
+++ generateList/makeList.php
@@ -9,7 +9,68 @@
 	else {
 		header( 'Location: ./GenerateList.php' ) ;
 	}
+	?>
+	<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<!-- BASIC SETUP INFORMATION -->
+<meta charset="utf-8">
+
+<!-- WEBPAGE DATA -->
+<title>Generate List</title>
+<meta name="description" content="generate list of food">
+<meta name="keywords" content="generate list, recipedia">
+<meta name="author" content="Tom, Adam, Ben">
+<link rel="shortcut icon" href="../websiteIcon.ico">
+
+<!-- REQUIRED FOR BOOTSTRAP AND JQUERY -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+
+<!-- CONTENT OF PAGE, CONSULT BOOTSTRAP STYLE GUIDE WHEN ASSIGNING CLASSES -->
+<body>
+
+<!-- TOP OF PAGE INFORMATION AND NAVIGATION -->
+<header>
+<!-- dummy navbar to give buffer for top of page -->
+<div class="navbar"></div>
+
+<!-- contains the navigation for the page -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <div class="navbar-brand"></div>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li ><a href="../index.php">Home</a></li>
+        <li ><a href="../editFoods/FoodList.php">Food List</a></li>
+        <li class="active"><a href="./generateList/GenerateList.php">Generate List</a></li>
+        <li class=""><a href="../login/loginPage.php">Login Page</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</header>
+
+<!-- MAIN CONTENT FOR THE PAGE GOES HERE -->
+<main>
+<div class="container">
+  <!-- profile image -->
+  <div class="row">
+    <div class="col-sm-3"></div>
+    <div class="col-sm-6 text-justify">
 	
+	<?php
 	$num = $_POST["numberOfMeals"];
 	
 	$servername = "localhost";
@@ -83,3 +144,6 @@
 
     // $conn->close();
 ?>
+
+</div>
+</main>
